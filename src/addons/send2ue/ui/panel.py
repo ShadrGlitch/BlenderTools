09641be2f448c09f28e008744d354b837738d454
Mordynak <shadrglitch@proton.me>
2025-06-26 13:50:40 +0100

    Cleaned up Panel Display

--- src/addons/send2ue/ui/panel.py
+++ src/addons/send2ue/ui/panel.py
@@ -2,37 +2,46 @@
 import bpy
 from ..constants import ToolInfo, Extensions
 
-class SEND2UE_PT_NPanel(bpy.types.Panel):
-    bl_label = "Send 2 UE"
-    bl_idname = "SEND2UE_PT_NPanel"
+class SEND2UE_PT_Export(bpy.types.Panel):
+    bl_label = "Export"
+    bl_idname = "SEND2UE_PT_Export"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "Send 2 UE"
 
     def draw(self, context):
         layout = self.layout
-        # Export section
-        layout.label(text="Export")
-        layout.operator('wm.send2ue', text="Send to Unreal")
-        layout.operator('wm.settings_dialog', text="Settings")
-        # Import section
-        layout.separator()
-        layout.label(text="Import")
+        layout.operator('wm.send2ue', text="Push Assets")
         layout.operator('wm.import_asset', text="Import Asset")
-        # Utilities section
-        layout.separator()
-        layout.label(text="Utilities")
+        layout.operator('wm.settings_dialog', text="Settings")
+
+
+class SEND2UE_PT_Utilities(bpy.types.Panel):
+    bl_label = "Utilities"
+    bl_idname = "SEND2UE_PT_Utilities"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Send 2 UE"
+
+    def draw(self, context):
+        layout = self.layout
         layout.operator('send2ue.create_predefined_collections', text="Create Predefined Collections")
         layout.operator('send2ue.start_rpc_servers', text="Start RPC Servers")
-        # Dynamic extension operators (if any)
         operator_namespace = getattr(bpy.ops, ToolInfo.NAME.value, None)
         if operator_namespace:
             for namespace in dir(operator_namespace):
                 if namespace.startswith(f'{Extensions.NAME}_'):
                     layout.operator(f'{ToolInfo.NAME.value}.{namespace}')
 
+classes = [
+    SEND2UE_PT_Export,
+    SEND2UE_PT_Utilities
+]
+
 def register():
-    bpy.utils.register_class(SEND2UE_PT_NPanel)
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 def unregister():
-    bpy.utils.unregister_class(SEND2UE_PT_NPanel)
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
